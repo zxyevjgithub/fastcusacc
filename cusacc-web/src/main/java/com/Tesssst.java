a77947a4785c0ab417ed7a553e158664f35982ba
daiweisong <daiweisong-it@bestpay.com.cn>
2019-11-13 17:23:13 +0800

    修复pom

--- cusacc-web/src/main/java/com/Tesssst.java
+++ cusacc-web/src/main/java/com/Tesssst.java
@@ -1,10 +1,22 @@
 package com;
 
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * @author devdws
  * @version Id: Tesssst.java, v 0.1  2019/8/1 12:57 devdws Exp $$
  */
-@Service
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:application.xml"})
+@Slf4j
 public class Tesssst {
+
+    @Test
+      public void  ddd(){
+        System.out.println(333);
+      }
+
 }
